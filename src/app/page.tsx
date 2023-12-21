8fae304aa875e5e0b447147e50832d9cceb72d74
nisharga <kabirnisharga@gmail.com>
2023-12-21 11:27:18 +0600

    hero area complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import MaxWidthWrapper from '@/components/MaxWidthWrapper'
+import { Button, buttonVariants } from '@/components/ui/button'
+import Link from 'next/link'
 import React from 'react'
 
 const Homepage = () => {
   return (
+    <>
     <MaxWidthWrapper>
     <div className='py-20 mx-auto text-center flex flex-col items-center max-w-3xl'>
           <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
@@ -15,8 +18,15 @@ const Homepage = () => {
           <p className='mt-6 text-lg max-w-prose text-muted-foreground'>Welcome to SmartHippo. Every asset on our
             platform is verified by our team to ensure our
             highest quality standards.</p>
+            <div className="flex flex-col sm:flex-row gap-4 mt-6">
+              <Link href="/product" className={buttonVariants()}>Browse Trending</Link>
+              <Button variant="ghost">Our quality promise &rarr;</Button> 
+            </div>
           </div>
+
+          {/* TODO List Product */}
     </MaxWidthWrapper>
+    </>
   )
 }
 
